7f4c8697396780763b74601bd13eb6865fb7142f
Nharshimha5 <84217211+Nharshimha5@users.noreply.github.com>
2021-05-15 10:14:53 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,65 @@
+#To print a string
+str="I'm a Developer"
+print(str)
+
+#To print a string1
+str1= "Hello world"
+print(str1)
+
+#To print an email
+print("Email:""nharshimha5@gmail.com")
+
+#To roundoff the given number
+a=80.90
+print(round(a))
+#floor function
+import math
+print(math.floor(a))
+
+#To print the caption
+caption="Success always Hugs you in Private, but Failure always Slaps you in the Public, that's Life."
+print(caption)
+
+#To convert the caption into uppercase
+print(caption.upper())
+#To convert the caption into lowercase
+print(caption.lower())
+
+print(caption.capitalize())
+
+#To print caption type
+print(type(caption))
+
+#create a list
+vegitables=["tomato","brinjal","cucumber","potato","snakegourd"]
+print(vegitables)
+
+#To append Apple fruit
+vegitables.append("apple")
+print(vegitables)
+
+#To append mango fruit
+vegitables.append("Mango")
+print(vegitables)
+
+#To append banana fruit
+vegitables.append("banana")
+print(vegitables)
+
+#To pop the element2
+vegitables.pop(2)
+print(vegitables)
+#To remove the specified element
+vegitables.remove("snakegourd")
+print(vegitables)
+
+#To create employee dictionary
+Dict={"name":"Prasanna","Id":12815,"salary":"30K","DOB":"1995"}
+print(Dict)
+
+
+
+
+
+
+
